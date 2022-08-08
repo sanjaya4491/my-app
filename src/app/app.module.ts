320497875c99060456c893b2b25dd8b04ab9fb7b
sanjaya4491 <54679398+sanjaya4491@users.noreply.github.com>
2022-08-08 00:53:08 -0500

    couple changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule, routingComponents} from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProjectsComponent } from './projects/projects.component';
+import { FooterComponent } from './footer/footer.component';
+import { SocialsComponent } from './socials/socials.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { BackgroundComponent } from './background/background.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     routingComponents,
-    ProjectsComponent
+    ProjectsComponent,
+    FooterComponent,
+    SocialsComponent,
+    NavigationComponent,
+    BackgroundComponent
   ],
   imports: [
     BrowserModule,
